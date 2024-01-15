67b26a52ca3e2dcf23929f707d6dcff718064a81
GlennMoreno <lolglenn26@gmail.com>
2024-01-15 11:45:15 +0800

    first commit

---
+++ praktek1/sudan.js
@@ -0,0 +1,12 @@
+const namaNegara = sudan;
+const benua = afrika;
+const sudanLang = Arab;
+let population =  47.958;
+let president = "AbdulFattah"
+let kepulauan = false
+
+console.log('NamaNegara:', namaNegara);
+console.log('Benua', benua);
+console.log('sudanLang:',sudanLang);
+console.log('population:',population);
+console.log('president:',president);
